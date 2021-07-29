8698792277da140c869f3efcf186c70bf2627ca5
Ndeah Paula <paula@Ndeahs-MacBook-Air.local>
2021-07-28 19:10:10 -0500

    fonts

--- app/src/main/java/com/androidesappc/main/MainActivitySplash.kt
+++ app/src/main/java/com/androidesappc/main/MainActivitySplash.kt
@@ -3,9 +3,14 @@ package com.androidesappc.main
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import android.widget.TextView
 
 class MainActivitySplash : AppCompatActivity() {
+
+    lateinit var topAnim: Animation
+    lateinit var bottonAnim: Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,7 +18,7 @@ class MainActivitySplash : AppCompatActivity() {
         val games:TextView = findViewById(R.id.textViewGames)
         games.typeface = Typeface.createFromAsset(assets, "fonts/earwig.ttf")
 
-
+        topAnim = AnimationUtils
 
 
 
