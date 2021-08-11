346b85943ce5bb9cfe18b6962e25808a3866bab7
Margge Guiza <marcelaguiza@gmail.com>
2021-08-11 18:18:19 -0400

    Rename splash activity

--- app/src/main/java/com/androidesappc/splash/SplashActivity.kt
+++ app/src/main/java/com/androidesappc/splash/SplashActivity.kt
@@ -1,24 +1,24 @@
-package com.androidesappc.main
+package com.androidesappc.splash
 
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.Animation
-import android.view.animation.AnimationUtils
 import android.widget.TextView
+import com.androidesappc.main.R
 
-class MainActivitySplash : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
 
     lateinit var topAnim: Animation
     lateinit var bottonAnim: Animation
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_splash)
+        setContentView(R.layout.activity_splash)
         val games:TextView = findViewById(R.id.textViewGames)
         games.typeface = Typeface.createFromAsset(assets, "fonts/earwig.ttf")
 
-        topAnim = AnimationUtils
+
 
 
 
