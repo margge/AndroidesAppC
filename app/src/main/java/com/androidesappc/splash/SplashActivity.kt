28a30561906d113bb4d7a7ff0b6ee204c526c0e5
Ndeah Paula <paula@Ndeahs-MacBook-Air.local>
2021-08-11 18:04:39 -0500

    finished the splash

--- app/src/main/java/com/androidesappc/splash/SplashActivity.kt
+++ app/src/main/java/com/androidesappc/splash/SplashActivity.kt
@@ -4,26 +4,41 @@ import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import android.widget.TextView
 import com.androidesappc.main.R
 
 class SplashActivity : AppCompatActivity() {
 
     lateinit var topAnim: Animation
-    lateinit var bottonAnim: Animation
+    lateinit var botAnim: Animation
+    lateinit var fan: ImageView
+    lateinit var games : TextView
+    lateinit var sub: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        val games:TextView = findViewById(R.id.textViewGames)
+         games = findViewById(R.id.textViewGames)
+         fan = findViewById(R.id.imageView)
+        sub = findViewById(R.id.TVsubheading)
         games.typeface = Typeface.createFromAsset(assets, "fonts/earwig.ttf")
 
+        topAnim = AnimationUtils.loadAnimation(this,R.anim.top_animation)
 
+        botAnim = AnimationUtils.loadAnimation(this, R.anim.bottom_animation)
 
-
-
-
-
+        loadAnimation ()
+    }
+    private fun loadAnimation() {
+        fan.animation = topAnim
+        games.animation = botAnim
+        sub.animation = topAnim
 
     }
+
+
+
+
 }
