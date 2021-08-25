b4c34c9e9d53634ae196cfc22c75dc4a6327d62b
Ndeah Paula <paula@Ndeahs-MacBook-Air.local>
2021-08-25 17:31:42 -0500

    change page

---
+++ app/src/main/java/com/androidesappc/main/MenuActivity.kt
@@ -0,0 +1,21 @@
+package com.androidesappc.main
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.androidesappc.splash.SplashActivity
+
+class MenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu)
+    }
+
+    private fun loadMenuScreen() {
+        val runnable = Runnable { startActivity(Intent(this, MenuActivity::class.java)) }
+        val handler = Handler(Looper.getMainLooper())
+        handler. postDelayed (runnable, 2000)
+    }
+}
